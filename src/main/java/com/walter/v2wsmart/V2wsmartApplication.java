0e3820ddbfbcca5ff7df66a4f2c34fb19501d183
waltinhoporfi <walterporfirio@hotmail.com>
2021-05-13 23:48:06 -0300

    commit inicial

---
+++ src/main/java/com/walter/v2wsmart/V2wsmartApplication.java
@@ -0,0 +1,13 @@
+package com.walter.v2wsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2wsmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2wsmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walter/v2wsmart/V2wsmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.v2wsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2wsmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
